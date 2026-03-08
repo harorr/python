f666bd18d8294b52abc252a07dee53b896c93c92
Harold Reina <haro.rr@hotmail.com>
2026-03-08 16:50:52 -0500

    Commit de archivos de aprendizaje

---
+++ numeros.py
@@ -0,0 +1,26 @@
+x = 1
+y = 10.5
+z = 3j
+
+print(type(z))
+print(type(y))
+print(type(x))
+
+positivo = 5
+negativo = -5
+
+imaginario = -5 + 3j
+
+xf = float(x)
+
+print(type(xf))
+print(xf)
+
+yf = int(y)
+
+print(type(yf))
+print(yf)
+
+import random
+
+print(random.randrange(1, 10))
---
+++ tipo_dato.py
@@ -0,0 +1,9 @@
+diccionario={
+    "Nombre":"Ari",
+    "Edad":34,
+    "Ciudad":"México"
+}
+print(diccionario)
+
+tupla=("a","b","c")
+print(tupla)
