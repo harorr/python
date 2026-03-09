bd5b6b406df29974f95fbb30abc78622a7fc34b2
Harold <haro.rr@hotmail.com>
2026-03-09 10:39:24 -0500

    Prueba de request a API

---
+++ texto.py
@@ -0,0 +1,13 @@
+
+import requests
+
+url = f"https://ghoapi.azureedge.net/api/WHOSIS_000001?$filter=SpatialDim eq 'COUNTRY' and TimeDimensionValue eq '2020' and Dim1 eq 'SEX_BTSX'" 
+
+fewer_country_codes = ["USA","MEX","CAN"]
+
+for country_code in fewer_country_codes:
+    r = requests.get(f"https://ghoapi.azureedge.net/api/WHOSIS_000001?$filter=SpatialDim eq '{country_code}' and TimeDimensionValue eq '2020' and Dim1 eq 'SEX_BTSX'")
+    result = r.json()
+    life_expectancy = result["value"][0]["NumericValue"]
+    print(country_code, life_expectancy)
+
