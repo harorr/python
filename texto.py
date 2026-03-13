571c8be9eb334a5ca41a0cb9be4bf1acd5edbce9
Harold Reina <haro.rr@hotmail.com>
2026-03-13 16:29:57 -0500

    texto y numeros

---
+++ texto.py
@@ -0,0 +1,23 @@
+print('Hola "Mundo"')
+
+ingles = "I'm Harold"
+
+multiples = """Hola
+Mundo"""
+
+print(ingles)
+print(multiples)
+
+palabra = "Murcielago"
+print(len(palabra))
+
+texto = "Esta es una prueba de fundamentos de python"
+estaIncluida = "Python" in texto
+noIncluida = "C++" not in texto
+
+print(estaIncluida)
+print(noIncluida)
+
+mayuscula = texto.upper()
+
+print(mayuscula)
